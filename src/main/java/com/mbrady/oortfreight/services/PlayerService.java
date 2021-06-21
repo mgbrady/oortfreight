df4d4601a3e375247ae949a25c4400fe9dca37f4
Michael Brady <michael.brady.g@gmail.com>
2021-06-21 14:39:05 -0400

    added player service

---
+++ src/main/java/com/mbrady/oortfreight/services/PlayerService.java
@@ -0,0 +1,39 @@
+package com.mbrady.oortfreight.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.mbrady.oortfreight.dao.IPlayerRepo;
+import com.mbrady.oortfreight.models.Player;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class PlayerService {
+    @Autowired
+    IPlayerRepo playerRepo;
+
+    public List<Player> getAllPlayers() {
+        return playerRepo.findAll();
+    }
+
+    public Optional<Player> findByName(String name) {
+        return playerRepo.findByPlayerName(name);
+    }
+
+    public Player getPlayerById(Long id) {
+        return playerRepo.getById(id);
+    }
+
+    public Player savePlayer(Player player) {
+        return playerRepo.save(player);
+    }
+
+    public Player removePlayer(Player player) {
+        playerRepo.delete(player);
+        return player;
+    }
+}
