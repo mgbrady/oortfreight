ce100fa083f62634895f4f751224e56f71b53fe1
Michael Brady <michael.brady.g@gmail.com>
2021-06-21 11:17:07 -0400

    initialized repository

---
+++ src/main/java/com/mbrady/oortfreight/OortfreightApplication.java
@@ -0,0 +1,13 @@
+package com.mbrady.oortfreight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OortfreightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OortfreightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbrady/oortfreight/OortfreightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbrady.oortfreight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OortfreightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
